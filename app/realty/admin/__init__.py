5cb8ee26708010998df9c42a87f3217dd3512b86
tlartem <tlartem@ya.ru>
2024-10-23 12:28:34 +0300

    M-16: Добавить админку для секций

--- app/realty/admin/__init__.py
+++ app/realty/admin/__init__.py
@@ -1,4 +1,5 @@
 from .FlatAdmin import FlatAdmin
 from .FloorAdmin import FloorAdmin
+from .SectionAdmin import SectionAdmin
 
-__all__ = ('FlatAdmin', 'FloorAdmin')
+__all__ = ('FlatAdmin', 'FloorAdmin', 'SectionAdmin')
