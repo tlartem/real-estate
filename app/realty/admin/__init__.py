77f9b045171487da16d7d31972972738d21c1a47
tlartem <tlartem@ya.ru>
2024-10-23 12:47:08 +0300

    M-19: Добавить админку для проектов

--- app/realty/admin/__init__.py
+++ app/realty/admin/__init__.py
@@ -1,6 +1,7 @@
 from .BuildingAdmin import BuildingAdmin
 from .FlatAdmin import FlatAdmin
 from .FloorAdmin import FloorAdmin
+from .ProjectAdmin import ProjectAdmin
 from .SectionAdmin import SectionAdmin
 
-__all__ = ('FlatAdmin', 'FloorAdmin', 'SectionAdmin', 'BuildingAdmin')
+__all__ = ('FlatAdmin', 'FloorAdmin', 'SectionAdmin', 'BuildingAdmin', 'ProjectAdmin')
